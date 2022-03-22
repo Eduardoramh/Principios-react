34eb7456aa0d8b0dbb8479ab852fc9f1d76731cd
Eduardoramh <lalo.ramirezh102@gmail.com>
2022-03-22 11:07:40 -0600

    Hook de efecto - useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const [stateCar, setStatecar] = useState(false);
+  const [contar, setContar] = useState(0);
+
+  useEffect(() =>{
+    console.log("Total: " + contar);
+  }, [contar]);
 
   const encenderApagar = () => {
     // setStatecar(!stateCar);
     setStatecar(prevValue => !prevValue);
+    setContar(contar + 1);
   };
 
   return (
@@ -15,6 +21,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h3>El coche esta: {stateCar ? "Encendido" : "Apagado"}</h3>
+        <h4>Clicks: {contar}</h4>
         <button onClick={encenderApagar}>Encender / Apagar</button>
       </header>
     </div>
